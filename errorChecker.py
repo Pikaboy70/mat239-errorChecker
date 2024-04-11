9ac572eaf686e5df7d71e7c5dcbc8f24842cbed6
Sam Danforth <71108003+Pikaboy70@users.noreply.github.com>
2024-04-11 18:48:05 -0400

    Migrated code into a class library
    Important: Do not run, will cause a recursion error. The cause is known, but will be fixed in the future.
    
    Functions from files have been moved to classes with the same original name. This allows much easier sharing of data between various functions.
    
    README has been updated.
    
    Some in-code comments need to be remade.

--- errorChecker.py
+++ errorChecker.py
@@ -1,25 +1,137 @@
 #!/usr/bin/env python3
 __author__ = "Sam Danforth"
 
+from random import randint
 
-# TODO: Migrate everything to the classes
+
 class ErrorChecker(object):
-    def __init__(self):
-        pass
+    @staticmethod
+    def calculateSuccess():
+        print(f"Noise level used: {NoisyChannel.noiseLevel}%")
+        print(f"Number of bad messages detected: {Receiver.failCount}")
+        print(f"Total number of messages generated: {Transmitter.messageCount}")
+        failRate: float = Receiver.failCount / Transmitter.messageCount
+        print(f"Fail rate: {failRate * 100:.3f}%")
 
 
 class Transmitter(ErrorChecker):
     generatedMessages: list[str] = []
+    preppedMessages: list[str] = []
+    messageCount: int = 0
 
-    def __init__(self):
-        super().__init__()
+    @staticmethod
+    def generateMessages(count: int = 10000):  # Count is the number of messages to generate, default 10,000
+        Transmitter.messageCount = count
+        for i in range(0, Transmitter.messageCount):  # Generate inputted number of messages total
+            message = randint(0, 255)  # Choose random 8-bit binary number (integer from 0-255)
+            # Remove extra characters added by binary conversion and fill with leading zeros
+            message = bin(message).replace("0b", "").zfill(8)
+            Transmitter.generatedMessages.append(message)
+        return Transmitter.generatedMessages
+
+    @staticmethod
+    def creatChecksums(messages: list[str]):
+        checksum: bool
+        for msg in messages:  # Iterate through message list
+            checksum = Transmitter.createChecksumSingle(msg)
+            if checksum:
+                msg = msg + "01"
+            else:
+                msg = msg + "00"
+            Transmitter.preppedMessages.append(msg)
+        return Transmitter.preppedMessages
+
+    @staticmethod
+    def createChecksumSingle(message: str):
+        count: int = 0
+        checksum: bool
+        for char in message:
+            if char == "1":
+                count += 1
+            elif char != "0":
+                raise ValueError("Message contains non-binary data")
+        if count % 2 == 1:
+            return False
+        elif count % 2 == 0:
+            return True
+
+    # TODO: Fix recursion error, find cause
+    @staticmethod
+    def getNewMessage(index: int):
+        message = Transmitter.preppedMessages[index]
+        garbledMessage = NoisyChannel.bitFlipperSingle(message, NoisyChannel.noiseLevel)
+        return garbledMessage
 
 
 class NoisyChannel(ErrorChecker):
-    def __init__(self):
-        super().__init__()
+    noiseLevel: int = 0
+    garbledMessages: list[str] = []
+
+    @staticmethod
+    def setNoiseLevel():
+        NoisyChannel.noiseLevel = randint(0, 100)
+
+    @staticmethod
+    def bitFlipper(messages: list[str]):
+        noiseLevel = NoisyChannel.noiseLevel
+        garbledMessages: list[str] = []
+        for msg in messages:
+            garbledMessage = NoisyChannel.bitFlipperSingle(msg, noiseLevel)
+            # Join modded character list with empty string to create new message
+            garbledMessages.append(garbledMessage)  # Add to edited messages list
+        return garbledMessages
+
+    @staticmethod
+    def bitFlipperSingle(message: str, noiseLevel: int):
+        index: int = 0
+        charList = list(message)
+        for char in charList:
+            randomInt = randint(0, 100)
+            if randomInt <= noiseLevel:
+                if char == "0":  # Change 0 to 1
+                    charList[index] = "1"
+                elif char == "1":  # Change 1 to 0
+                    charList[index] = "0"
+                else:
+                    raise ValueError("Message contains non-binary data.")
+            index += 1
+        garbledMessage = ''.join(charList)
+        return garbledMessage
 
 
+# TODO: Request new message when bad
 class Receiver(ErrorChecker):
-    def __init__(self):
-        super().__init__()
+    acceptedMessages: list[str] = []
+    failCount: int = 0
+
+    @staticmethod
+    def goodMessage(message: str):
+        Receiver.acceptedMessages.append(message)
+
+    @staticmethod
+    def badMessage(index: int):
+        Receiver.failCount += 1
+        newMessage = Transmitter.getNewMessage(index)
+        Receiver.analyzeSingleMessage(newMessage, index)
+
+    @staticmethod
+    def analyzeMessages(messages: list[str]):
+        index: int = 0
+        for message in messages:
+            Receiver.analyzeSingleMessage(message, index)
+            index += 1
+
+    @staticmethod
+    def analyzeSingleMessage(message: str, index: int):
+        ackBit = message[8]
+        if ackBit == "1":
+            Receiver.badMessage(index)
+        messageData = message[:8]
+        checksum = [9]
+        expectedChecksum = Transmitter.createChecksumSingle(messageData)
+        if expectedChecksum and checksum == "1":
+            Receiver.goodMessage(message)
+        elif not expectedChecksum and checksum == "0":
+            Receiver.goodMessage(message)
+        else:
+            Receiver.badMessage(index)
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # !/usr/bin/env python3
 __author__ = "Sam Danforth"
-import noisyChannel
-import receiver
-import transmitter
+from errorChecker import *
 
 
 # TODO: Create ErrorChecker class if time permits
 def main():
-    nums: list[str] = transmitter.generateMessages()
-    nums = transmitter.createChecksums(nums)
-    garbledMessages = noisyChannel.bitFlipper(nums)
-    goodMessage = receiver.analyzeMessages(garbledMessages)
-    print(f"Good message? {goodMessage}")
+    messages = Transmitter.generateMessages()
+    messages = Transmitter.creatChecksums(messages)
+    NoisyChannel.setNoiseLevel()
+    messagesToSend = NoisyChannel.bitFlipper(messages)
+    Receiver.analyzeMessages(messagesToSend)
+    ErrorChecker.calculateSuccess()
+    pass
 
 
 if __name__ == "__main__":
-    main()  # Do not run, might return something but will be far from correct
+    main()  # Do not run, will throw RecursionError
